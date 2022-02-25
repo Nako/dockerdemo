c3ea30d69e5cde00fb2eee62c5e090908b682970
nakoatdocker1vm <bashnako@gmail.com>
2022-02-25 10:09:28 +0200

    Changed configuration settings

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
-   $database = "";
-   $user = "";
-   $password  = "";
-   $host = "";
+    $database = "bulgaria";
+    $user = "web_user";
+    $password = "Password1";
+    $host = "db";
 ?>
